722c7b173b84a2096d370e40d20db6e632272c4d
vyadav22 <vikas.yadav@student.dit.ie>
2014-10-30 17:10:20 +0000

    added euler problems

--- python-flask/my_application/app.py
+++ python-flask/my_application/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+import os, sys
 app = Flask(__name__)
 
 @app.route("/")
@@ -24,3 +25,26 @@ def list_files():
    for file in dirs:
     files = str(files + file + ", ")
     return files
+
+@app.route("/euler1")
+def euler1():
+  sum = 0
+  for i in range (1, 1000):
+    if i%3 or i%5:
+      sum = sum + i
+    return ("Euler 1:The total is "sum)
+
+
+@app.route("/euler2")
+def euler2():
+  t1 = 1
+  t2 = 2
+  total = 0
+  sum = 2
+  while(total < 4000000):
+    total = t1 + t2
+    t1 = t2
+    term2 = total
+    if(total%2 = 0):
+      sum = sum  +total
+   return("Euler 2: The total is ",sum)
